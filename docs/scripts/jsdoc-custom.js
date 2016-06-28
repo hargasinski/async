eb1e60bc497db9cbffd82ccc6738359e03860810
Hubert Argasinski <argasinski.hubert@gmail.com>
2016-06-28 02:56:39 -0400

    rebuilt docs, final version before publishing?

--- docs/scripts/jsdoc-custom.js
+++ docs/scripts/jsdoc-custom.js
@@ -1,15 +1,54 @@
+function matchSubstrs(methodName) {
+    var tokens = [];
+
+    var len = methodName.length;
+
+    for (var size = 1; size <= len; size++){
+        for (var i = 0; i+size<= len; i++){
+            tokens.push(methodName.substr(i, size));
+        }
+    }
+
+    return tokens;
+}
+
 $(function initSearchBar() {
     var methodNames = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        datumTokenizer: matchSubstrs,
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         prefetch: './methodNames.json'
     });
 
     var sourceFiles = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        datumTokenizer: matchSubstrs,
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         prefetch: './sourceFiles.json'
     });
+
+    var githubIssues = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: 'https://api.github.com/search/issues?q=%QUERY+repo:caolan/async',
+            cache: true,
+            wildcard: '%QUERY',
+            transform: function(response) {
+                return $.map(response.items, function(issue) {
+                    // if (issue.state !== 'open') {
+                    //     return null;
+                    // }
+                    return {
+                        url: issue.html_url,
+                        name: issue.number + ': ' + issue.title,
+                        number: issue.number
+                    };
+                }).sort(function(a, b) {
+                    return b.number - a.number;
+                });
+            }
+        }
+    });
+
 
     $('.typeahead').typeahead({
         hint: true,
@@ -20,7 +59,7 @@ $(function initSearchBar() {
         name: 'Methods',
         source: methodNames,
         templates: {
-            header: '<h3 class="search-bar-header">Methods</h3>'
+            header: '<h3 class="search-bar-header-first">Methods</h3>'
         }
     },
     {
@@ -29,8 +68,30 @@ $(function initSearchBar() {
         templates: {
             header: '<h3 class="search-bar-header">Source Files</h3>'
         }
+    },
+    {
+        name: 'Issues',
+        source: githubIssues,
+        display: 'name',
+        templates: {
+            header: '<h3 class="search-bar-header">Issues</h3>'
+        }
     }).on('typeahead:select', function(ev, suggestion) {
-        var $el = document.getElementById('.' + suggestion)
-        $('html, body').animate({ scrollTop: $el.offsetTop }, 500);
+        var protocol = window.location.protocol
+        var host = window.location.host;
+        var currentPath = window.location.pathname;
+        // handle issues
+        if (typeof suggestion !== 'string') {
+            window.location.href = suggestion.url;
+        // handle source files
+        } else if (suggestion.indexOf('.html') !== -1) {
+            window.location.href = protocol + '//' + host + '/' + suggestion;
+        // handle searching from one of the source files or the home page
+        } else if (currentPath !== '/docs.html') {
+            window.location.href = protocol + '//' + host + '/docs.html#.' + suggestion;
+        } else {
+            var $el = document.getElementById('.' + suggestion);
+            $('#main').animate({ scrollTop: $el.offsetTop }, 500);
+        }
     });
 });
